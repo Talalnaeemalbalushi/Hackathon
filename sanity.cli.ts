488ccaa5fbcbfb64f361300f26dcc0afa8fc0695
Talalnaeemalbalushi <talalnaeemalbalushi@gmail.com>
2025-02-08 00:12:25 +0500

    Add files via upload

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,13 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
-import { defineCliConfig } from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({
+  api: {
+    projectId: 'wm9rbp33',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+})
